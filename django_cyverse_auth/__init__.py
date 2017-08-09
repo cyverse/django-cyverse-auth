623b13c01c06921d78ce4fee5f4ef2477fa6150e
Steve Gregory <contact@steve-gregory.com>
2017-08-09 10:10:47 -0700

    Minor version bump (1.1.1) -- Include optimiations to the Openstack/Keystone login and authBackend

--- django_cyverse_auth/__init__.py
+++ django_cyverse_auth/__init__.py
@@ -2,5 +2,5 @@
 from collections import namedtuple
 
 version_info = namedtuple("version_info", ["major", "minor", "patch"])
-version = version_info(1, 1, 0)
+version = version_info(1, 1, 1)
 __version__ = "{0.major}.{0.minor}.{0.patch}".format(version)
